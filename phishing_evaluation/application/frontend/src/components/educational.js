3c9ac04b765f10db1b9ece0fe366a090b6a874f6
Jess Phelan <jessiephelan22@gmail.com>
2024-02-27 11:11:41 -0600

    added kates changes to score page and added new human generated emails

---
+++ phishing_evaluation/application/frontend/src/components/educational.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Educational = () => {
+  return (
+    <div>
+      <h1>Improving Detection</h1>
+      <p>Here is some content to help you get better at detecting phishing emails.</p>
+    </div>
+  );
+};
+
+export default Educational;
