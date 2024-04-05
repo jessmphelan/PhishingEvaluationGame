9a5b9439d41af4a9f10599fd39d3e3620346a4ab
Jess Phelan <jessiephelan22@gmail.com>
2024-04-04 19:30:40 -0500

    re-generating emails

--- phishing_evaluation/application/backend/data/scripts/gpt_email_generation/config.py
+++ phishing_evaluation/application/backend/data/scripts/gpt_email_generation/config.py
@@ -1,11 +1,3 @@
-"""
-This allows us to load our environment variables from a .env file located in the same directory as the config.py file. 
-By calling load_dotenv() twice, it first attempts to load environment variables from any .env file found in the default location, 
-then specifically loads from the .env file at the path constructed with ENV_PATH. This ensures that the environment variables 
-OPENAI_API_KEY and ORGANIZATION are accessible in your Python script
-"""
-
-
 
 from dotenv import load_dotenv
 import os
